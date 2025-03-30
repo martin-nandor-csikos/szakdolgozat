2391c672d097d3afade3a8a7ab839453744fcea2
Csikos Martin Nándor <csikos.martin17@gmail.com>
2025-03-30 18:59:15 +0200

    creating constants in website module

---
+++ src/website/constants.py
@@ -0,0 +1,8 @@
+HTML_LINK_TAG = "a"
+HTML_HREF = "href"
+SLASH = "/"
+PDF_FILE_EXTENSION = ".pdf"
+BEAUTIFULSOUP_HTML_PARSER = "html.parser"
+WEBPAGE_EXTENSIONS: set[str] = {"html", "htm", "php", "asp", "aspx", "jsp"}
+EXTENSION_DOT = "."
+EMAIL_REGEX = r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)"
