2508149bdbcbd9e10683becc0fc092c70e9cd5d9
Csikos Martin Nándor <csikos.martin17@gmail.com>
2025-04-04 01:15:53 +0200

    simplified and ordered imports

--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
-from website.website import WebsiteInfo
-import sys
-import website
+from website import WebsiteInfo, parse_all
 import json
+import sys
 
 
 def main() -> None:
-    website_to_parse: str = sys.argv[1]
-    number_of_sites_to_visit = int(sys.argv[2])
+    """Main function of the program where the individual methods are called."""
 
-    website_info: WebsiteInfo = website.parse_all(
-        website_to_parse, number_of_sites_to_visit
-    )
+    website_to_parse: str = sys.argv[1]
+    number_of_sites_to_visit: int = int(sys.argv[2])
+
+    website_info: WebsiteInfo = parse_all(website_to_parse, number_of_sites_to_visit)
     website_info_json: str = json.dumps(website_info.to_dict(), indent=4)
     print(website_info_json)
 
