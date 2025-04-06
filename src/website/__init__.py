0ba2b95b04662a49f712d84cbebd5f31e2c8f56c
Csikos Martin Nándor <csikos.martin17@gmail.com>
2025-04-06 13:04:11 +0200

    added every currently existing method

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -1,1 +1,8 @@
-from .website import WebsiteInfo, parse_all
+from .website import (
+    parse,
+    parse_all,
+    get_links_from_html_content,
+    parse_for_emails,
+    is_file_url,
+    WebsiteInfo,
+)
