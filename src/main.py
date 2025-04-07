a4325f8bf3bd454db73fd2a3495c09d8462fbd49
Csikos Martin Nándor <csikos.martin17@gmail.com>
2025-04-07 02:05:38 +0200

    added utf8 compatibility for json

--- src/main.py
+++ src/main.py
@@ -10,7 +10,9 @@ def main() -> None:
     number_of_sites_to_visit: int = int(sys.argv[2])
 
     website_info: WebsiteInfo = parse_all(website_to_parse, number_of_sites_to_visit)
-    website_info_json: str = json.dumps(website_info.to_dict(), indent=4)
+    website_info_json: str = json.dumps(
+        website_info.to_dict(), indent=4, ensure_ascii=False
+    )
     print(website_info_json)
 
 
