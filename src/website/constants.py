79bfef2568058250c0d6abebb66841df2cb03f58
Csikos Martin Nándor <csikos.martin17@gmail.com>
2025-04-16 22:31:47 +0200

    added new constants for name parsing

--- src/website/constants.py
+++ src/website/constants.py
@@ -2,6 +2,7 @@ HTML_LINK_TAG = "a"
 HTML_HREF = "href"
 HTML_ID = "#"
 HTML_CLASS = "."
+HU_TOP_LEVEL_DOMAIN = "hu"
 SLASH = "/"
 SPACE = " "
 PDF_FILE_EXTENSION = ".pdf"
@@ -9,3 +10,41 @@ BEAUTIFULSOUP_HTML_PARSER = "html.parser"
 WEBPAGE_EXTENSIONS: set[str] = {"html", "htm", "php", "asp", "aspx", "jsp"}
 EXTENSION_DOT = "."
 EMAIL_REGEX = r"([a-zA-Z0-9_.+-]+@(?:[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*\.)+[a-zA-Z]{2,})"
+SPACY_LANGUAGE_HUNGARIAN = "hu"
+SPACY_LANGUAGE_ENGLISH = "en"
+SPACY_ENTITY_PERSON_HUNGARIAN = "PER"
+SPACY_ENTITY_PERSON_ENGLISH = "PERSON"
+NAME_REGEX = r"^([A-Z][a-záéíóöőúüű]*(?:-[A-Z][a-záéíóöőúüű]*)*(?: [A-Z][a-záéíóöőúüű]*(?:-[A-Z][a-záéíóöőúüű]*)*)+)$"
+HTML_TEXT_TAGS: set[str] = {
+    "p",
+    "a",
+    "span",
+    "div",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "li",
+    "td",
+    "th",
+    "label",
+    "button",
+    "strong",
+    "em",
+    "b",
+    "i",
+    "u",
+    "small",
+    "mark",
+    "blockquote",
+    "cite",
+    "code",
+    "pre",
+    "abbr",
+    "q",
+    "time",
+    "output",
+    "summary",
+}
